01b9aab175042e46fb9a6eb15ead30d7c6f62639
liuyuting220 <1369087854@qq.com>
2021-08-31 19:19:21 +0800

    解决工具框点击画布全部清除问题，增加画笔宽度

--- src/components/Canvaspage/index.jsx
+++ src/components/Canvaspage/index.jsx
@@ -14,26 +14,30 @@ import './index.css'
 export default function Canvaspage() {
     const electron = window.electron;
     let canvasIcon = true;
-    // let  = false
 
-    let [pencileBoxSta,setpencileBoxSta] = useState(false)
+    // 应用工具箱的按钮状态
+    const [pencileBoxSta,setpencileBoxSta] = useState(false)
     const canvasRef = useRef(null);
+    const inputRef = useRef(null);
+    const inputTextRef = useRef(null);
     
     let imgRef = '';       //图片保存字符串
-
-
+    
+    // let inputNum = inputTextRef.current;
+    // let input = inputRef.current;
     useEffect(() => {
+        // changeInputNum();
+        // inputTextRef.current = inputRef.current.value
         electron.ipcRenderer.send('canvas-start','');
+        // 接收参数 接收文件夹中的图片；列表
         electron.ipcRenderer.on('canvas-img',(event,arg) => {
             imgRef = arg;
             loadImg(arg);
         })
         
-    })
-
+    }, [])
+    // 初始化canvas画布及图片背景
     const loadImg = (arg) => {
-        // console.log(canvasIcon);
-
         const elem = canvasRef.current;
         const canvas = elem.getContext('2d');
 
@@ -53,8 +57,15 @@ export default function Canvaspage() {
         }
 
     }
+
+    // 改变画笔宽度数据
+    const changeInputNum = () => {
+        inputTextRef.current.innerHTML = inputRef.current.value
+    }
+
+    // 画笔点击
     const draw_pencile = ()=>{
-        console.log("pencle");
+        // console.log("pencle");
         pencile_detail("pencle",1,"#999")
     };
 
@@ -63,10 +74,8 @@ export default function Canvaspage() {
         const canvas = elem.getContext('2d');
         canvas.lineWidth = width;
         canvas.strokeStyle = color
-        console.log("111");
+        // console.log("111");
         elem.onmousedown = function(ev) {
-            // console.log("mousedown");
-            // console.log(ev.offsetY);
             var ev = ev || window.event;
             let offX = ev.offsetX;
             let offY = ev.offsetY;
@@ -92,7 +101,7 @@ export default function Canvaspage() {
         <div>
             <div className="draw_pencile_box">
                 {/* 应用工具箱 */}
-                <ApplicationTwo theme="outline" size="28" fill="#fff" strokeWidth={3} strokeLinecap="butt" onClick={() =>{let sta = !pencileBoxSta; setpencileBoxSta(sta);console.log(pencileBoxSta);}}/>
+                <ApplicationTwo theme="outline" size="28" fill="#fff" strokeWidth={3} strokeLinecap="butt" onClick={() =>{setpencileBoxSta(!pencileBoxSta);console.log(pencileBoxSta);}}/>
                 <div className={["pencile_wrap",pencileBoxSta?"pencile_wrap_time":"pencile_wrap_leave"].join(" ")}>
                     <ul className="pencile_ul">
                         <li className="pencile_li" onClick={draw_pencile}>
@@ -120,6 +129,10 @@ export default function Canvaspage() {
                             <Platte theme="outline" size="28" fill="#fff" strokeWidth={3} strokeLinecap="butt"/>
                         </li>
                     </ul>
+                    <div className="inputbox">
+                        <span ref = {inputTextRef}>{inputRef.current?.value}</span>
+                        <input type="range" min="1" max="30" step="1" ref={inputRef} onChange={()=>changeInputNum()}></input>
+                    </div>
                 </div>
             </div>
             <div>
